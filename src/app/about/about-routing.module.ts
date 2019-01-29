16b0209d2c7d60a1e211982f05e96c2a0dd062fc
Alberto Basalo <albertobasalo@hotmail.com>
2019-01-29 13:35:26 +0100

    2.3

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { InfoComponent } from './about/info/info.component';
+import { LinksComponent } from './about/links/links.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AboutComponent
+    component: AboutComponent,
+    children: [
+      {
+        path: 'links',
+        component: LinksComponent
+      },
+      {
+        path: 'info',
+        component: InfoComponent
+      }
+    ]
   }
 ];
 
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
+import { LinksComponent } from './about/links/links.component';
+import { InfoComponent } from './about/info/info.component';
 
 @NgModule({
-  declarations: [AboutComponent],
+  declarations: [AboutComponent, LinksComponent, InfoComponent],
   imports: [
     CommonModule,
     AboutRoutingModule
