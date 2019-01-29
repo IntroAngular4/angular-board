20e6b4852b38771c635875ceb83933b6f705a889
Alberto Basalo <albertobasalo@hotmail.com>
2019-01-29 13:19:00 +0100

    2-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: './home/home.module#HomeModule'
+  },
+  {
+    path: 'about',
+    loadChildren: './about/about.module#AboutModule'
+  },
+  {
     path: 'heroes',
     component: HeroesComponent
   },
