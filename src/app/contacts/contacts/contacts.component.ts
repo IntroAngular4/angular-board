bcf6fff09e4a2645fbc5fab257bfa362507cb7fe
Alberto Basalo <albertobasalo@hotmail.com>
2019-01-31 13:29:05 +0100

    4.4

--- src/app/contacts/contacts/contacts.component.ts
+++ src/app/contacts/contacts/contacts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from './models/contact.model';
+import { Option } from './models/option.model';
 
 @Component({
   selector: 'app-contacts',
@@ -8,25 +10,41 @@ import { Component, OnInit } from '@angular/core';
 export class ContactsComponent implements OnInit {
   public header = 'Contacts';
   public description = 'Manage your contact list';
-  public numContacts = 0;
+  public numContacts: number;
   public counterClass = 'tag secondary';
   public formHidden = false;
 
-  public workStatuses = [
-    { id: 0, description: 'unknow' },
-    { id: 1, description: 'student' },
-    { id: 2, description: 'unemployed' },
-    { id: 3, description: 'employed' }
-  ];
-  public contact = {
-    name: '',
-    isVIP: false,
-    gender: '',
-    workStatus: '0',
-    company: '',
-    education: ''
-  };
+  public workStatuses: Option[];
+  public contact: Contact;
+  public contacts: Contact[];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.workStatuses = [
+      { id: 0, description: 'unknow' },
+      { id: 1, description: 'student' },
+      { id: 2, description: 'unemployed' },
+      { id: 3, description: 'employed' }
+    ];
+    this.contact = {
+      name: '',
+      isVIP: false,
+      gender: '',
+      workStatus: 0,
+      company: '',
+      education: ''
+    };
+    this.numContacts = 0;
+    this.contacts = [];
+  }
+
+  public saveContact() {
+    this.contacts.push({ ...this.contact });
+    this.numContacts = this.contacts.length;
+  }
+
+  public deleteContact(contact: Contact) {
+    this.contacts = this.contacts.filter(c => c.name !== contact.name);
+    this.numContacts = this.contacts.length;
+  }
 }
