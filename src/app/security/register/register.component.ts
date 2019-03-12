2d8dcfa05548f165899c21fce73ebfaa21fd6c49
Alberto Basalo <albertobasalo@hotmail.com>
2019-03-12 13:06:44 +0100

    8.2

--- src/app/security/register/register.component.ts
+++ src/app/security/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -18,11 +18,41 @@ export class RegisterComponent implements OnInit {
     const dateLength = 10;
     const today = new Date().toISOString().substring(0, dateLength);
     const name = 'JOHN DOE';
+    const minPassLength = 4;
     this.formGroup = this.formBuilder.group({
       registeredOn: today,
-      name: name.toLowerCase(),
-      email: 'john@angular.io',
-      password: ''
+      name: [name.toLowerCase(), Validators.required],
+      email: ['john@angular.io', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(minPassLength), this.validatePassword]
+      ]
     });
+  }
+
+  private validatePassword(control: AbstractControl) {
+    const password = control.value;
+    let error = null;
+    if (!password.includes('$')) {
+      error = { ...error, dollar: 'needs a dollar symbol' };
+    }
+    if (!parseFloat(password[0])) {
+      error = { ...error, number: 'must start with a number' };
+    }
+    return error;
+  }
+
+  public register() {
+    const user = this.formGroup.value;
+    console.log(user);
+  }
+
+  public getError(controlName: string): string {
+    let error = '';
+    const control = this.formGroup.get(controlName);
+    if (control.touched && control.errors != null) {
+      error = JSON.stringify(control.errors);
+    }
+    return error;
   }
 }
